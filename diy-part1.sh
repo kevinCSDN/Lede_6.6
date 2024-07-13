b740b3019393bac4ea995251f7b193bfb55a67be
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2024-07-13 20:38:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -29,6 +29,7 @@ rm -rf ./feeds/packages/net/mosdns
 rm -rf feeds/packages/net/v2ray-geodata
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/themes/luci-theme-bootstrap
+rm -rf feeds/luci/themes/luci-app-argon-config
 
 #passwall2
 #git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
